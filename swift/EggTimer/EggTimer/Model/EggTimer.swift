c4f880a1f55ff988d6401fd484112a41a561c7b4
Maya Rosmarin <maya.rosmarin@gmail.com>
2021-06-24 19:36:25 -0600

    buttons tec

---
+++ swift/EggTimer/EggTimer/Model/EggTimer.swift
@@ -0,0 +1,81 @@
+//
+//  EggTimer.swift
+//  EggTimer
+//
+//  Created by Maya Rosmarin on 6/24/21.
+//
+
+import Foundation
+
+protocol EggTimerProtocol {
+    func timeRemainingOnTimer(_ timer: EggTimer, timeRemaining: TimeInterval)
+    func timerHasFinished(_ timer: EggTimer)
+}
+
+class EggTimer {
+    var timer: Timer? = nil
+    var startTime: Date?
+    var duration: TimeInterval = 360
+    var elapsedTime: TimeInterval = 0
+    var delegate: EggTimerProtocol?
+    
+    var isStopped: Bool {
+        return timer == nil && elapsedTime == 0
+    }
+    var isPaused: Bool {
+        return timer == nil && elapsedTime > 0
+    }
+    
+    func startTimer() {
+        startTime = Date()
+        elapsedTime = 0
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+            timer in self.timerAction()
+        }
+        timerAction()
+    }
+    
+    func resumeTimer() {
+        startTime = Date(timeIntervalSinceNow: -elapsedTime)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+            timer in self.timerAction()
+        }
+        timerAction()
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        
+        timerAction()
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+        timer = nil
+        
+        startTime = nil
+        duration = 360
+        elapsedTime = 0
+        
+        timerAction()
+    }
+    
+    func timerAction() {
+        guard let startTime = startTime else {
+            return
+        }
+        
+        elapsedTime = -startTime.timeIntervalSinceNow
+        
+        let secondsRemaining = (duration - elapsedTime).rounded()
+        if secondsRemaining <= 0 {
+            resetTimer()
+            delegate?.timerHasFinished(self)
+        } else {
+            delegate?.timeRemainingOnTimer(self, timeRemaining: secondsRemaining)
+        }
+    }
+}
